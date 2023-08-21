64b582ab3609d9e234062d581a3a2bf171e46e17
ruanvsrateira <vruan85@gmail.com>
2023-08-21 11:44:01 -0300

    chore(tailwind): modify theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,11 @@ export default {
         'montSerrat': ['Montserrat', 'sans-serif']
       },
       colors: {
-        'brand': '#574AE8',
-        'brand-blue': '#3EA1DB',
-        'dark-10': '#1A202C',
-        'dark-20': '#717171',
-        'dark-30': '#F3F5F7',
-        'dark-40': '#FFFFFF',
-        'dark-50': '#1A202C',
-        'support-1': '#2962FF',
-        'support-2': '#0BB07B',
-        'support-3': '#FFCE52',
-        'support-4': '#F03D3D',
-        'support-5': '#E0E0E0',
+        'blue': '#00AEFF',
+        'lightSilver': '#EBE9EA',
+        'extraLightSilver': '#F9F9F9',
+        'darkGray': '#181818',
+        'gray': '#474747',
       },
       screens: {
         'sm': '400px'
